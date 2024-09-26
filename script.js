e3bd9388d4f2abbe4a23ad7bba6918df1dfe2ecd
SJBM1876 <137792828+SJBM1876@users.noreply.github.com>
2024-09-26 15:34:36 +0100

    if/else statement for heart toggle

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let heart = document.querySelector("#like")
-heart.addEventListener("click", function (event) {
-    heart.src = "https://i2.pngimg.me/thumb/f/720/m2i8H7d3Z5b1b1b1.jpg"
-})
+let heart = document.querySelector("#like");
+
+heart.addEventListener("click", function () {
+
+    if (heart.src === "https://i2.pngimg.me/thumb/f/720/m2i8H7d3Z5b1b1b1.jpg") {
+    heart.src = "https://w7.pngwing.com/pngs/387/843/png-transparent-favorite-heart-like-likes-love-loved-basic-ui-2-line-icon.png"; // Image for "unlike"
+    } else {
+    heart.src = "https://i2.pngimg.me/thumb/f/720/m2i8H7d3Z5b1b1b1.jpg"; // Image for "like"
+    }
+});
